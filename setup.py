56b5b5e2821e9d0d3d2beb7714d8f8ebc9ad3262
Taylor B <43686206+tayjaybabee@users.noreply.github.com>
2021-11-28 21:43:39 -0600

    Add spanners sub-package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Inspyre-Toolbox',
-    version='1.0-alpha7',
+    version='1.0-alpha8',
     packages=['inspyre_toolbox', 'inspyre_toolbox.live_timer'],
     url='https://github.com/tayjaybabee/Inspyre-Toolbox',
     license='WTFPL',
