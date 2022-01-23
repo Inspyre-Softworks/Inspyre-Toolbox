c7ad0a708c66d6d10fbc181fe45c761f2acf09ce
Taylor B <43686206+tayjaybabee@users.noreply.github.com>
2022-01-22 21:36:32 -0600

    Version bump 1.1.4 -> 1.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Inspyre-Toolbox",
-    version="1.1.4",
+    version="1.1.5",
     packages=["inspyre_toolbox", "inspyre_toolbox.live_timer"],
     url="https://github.com/tayjaybabee/Inspyre-Toolbox",
     license="WTFPL",
