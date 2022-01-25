ed34854816b84499062144bc22b15fd2a5ffa0bb
Taylor B <43686206+tayjaybabee@users.noreply.github.com>
2022-01-24 20:20:10 -0600

    Version Bump
    New Packages:
    
      * inspyre_toolbox.syntactic_sweets
    
        * suppress_stdout(): A file object that dumps your stdout to /dev/null while in-use

---
+++ inspyre_toolbox/syntactic_sweets/__init__.py
@@ -0,0 +1,20 @@
+﻿#  Copyright (c) 2021. Taylor-Jayde Blackstone <t.blackstone@inspyre.tech> https://inspyre.tech
+"""
+
+A package containing decorators that are a quick help in programming with Python
+
+"""
+from contextlib import contextmanager
+from os import devnull
+import sys
+
+@contextmanager
+def suppress_stdout():
+    with open(devnull, "w") as dn:
+        old_stdout = sys.stdout
+        sys.stdout = dn
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+
