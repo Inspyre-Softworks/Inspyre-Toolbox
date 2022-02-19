7525ec7d856c5fcc95e71b7626b784505a0bf9a8
Taylor-Jayde Blackstone <t.blackstone@gmail.com>
2022-02-19 00:39:47 -0600

    Version bump v1.2
    v1.2a1 --> v1.2 (stable)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Inspyre-Toolbox",
-    version="1.1.5",
+    version="1.2",
     packages=["inspyre_toolbox", "inspyre_toolbox.live_timer"],
     url="https://github.com/tayjaybabee/Inspyre-Toolbox",
     license="WTFPL",
