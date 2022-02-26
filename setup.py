9110a5187a67820fe5f01863e5a44ef19f1b2195
Taylor B <tayjaybabee@gmail.com>
2022-02-26 00:46:29 -0600

    general bugfixes (#34)
    bump v1.2.1-alpha.3 -> v1.2.1-alpha.4
    
    Co-authored-by: Taylor-Jayde Blackstone <t.blackstone@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Inspyre-Toolbox",
-    version="1.2.1-alpha.3",
+    version="1.2.1-alpha.4",
     packages=["inspyre_toolbox", "inspyre_toolbox.live_timer"],
     url="https://github.com/tayjaybabee/Inspyre-Toolbox",
     license="WTFPL",
