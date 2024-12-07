41397cfaf28e9d55db198a3fab81a682ee0537ad
Taylor B. <tayjaybabee@gmail.com>
2024-12-07 11:37:37 -0600

    (feat) - Improve exceptional logging with skip print feature.

--- inspyre_toolbox/exceptional/__init__.py
+++ inspyre_toolbox/exceptional/__init__.py
@@ -12,19 +12,42 @@ class CustomRootException(Exception):
 
     default_message = 'An unknown error has occurred.'
 
-    def __init__(self, message: Optional[str] = None):
+    def __init__(self, message: Optional[str] = None, skip_print: bool = False):
         """
 
         Args:
             message:
         """
         super().__init__(message or self.default_message)
+        self.__printed = False
+        self._skip_print = None
         self.message = message or self.default_message
-        self.print_rich_panel()
+
+        self.skip_print = skip_print
+
+        if not self.skip_print:
+            self.print_rich_panel()
+
+    @property
+    def printed(self):
+        return self.__printed
+
+    @property
+    def skip_print(self):
+        return self._skip_print
+
+    @skip_print.setter
+    def skip_print(self, new):
+        if not isinstance(new, bool):
+            raise ValueError('skip_print must be a boolean.')
+
+        if not self.printed:
+            self._skip_print = new
 
     def print_rich_panel(self):
         console = Console()
         console.print(self.__rich__())
+        self.__printed = True
 
     def __rich__(self):
         return Panel(
